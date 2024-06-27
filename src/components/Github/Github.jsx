24c5ac972ae410eefb2384285e70f9c262d6e9c1
shubhin151 <60954538+shubh-151@users.noreply.github.com>
2024-06-27 15:42:44 +0530

    All commit

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -15,7 +15,7 @@ const Github = () => {
 
   return (
     <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
-      Github followers:{data.followers}
+      Github followers: ({data.followers})
       <img src={data.avatar_url} alt="Git picture" width={300} />
     </div>
   );
